23f9213a2bee04a9b3d62826e9665145dd661c1e
Majd ALhasan <94524369+majdalhasan@users.noreply.github.com>
2023-05-08 10:49:43 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let toggler = document.querySelector(".toggle");
+let nav = document.querySelector("nav");
+let close = document.querySelector(".close");
+
+toggler.onclick = function () {
+  nav.classList.add("open");
+};
+
+close.onclick = function () {
+  this.parentElement.classList.remove("open");
+};
+
+document.onkeyup = function (e) {
+  // console.log(e);
+  if (e.key === "Escape") {
+    nav.classList.remove("open");
+  }
+};
